646efb3ab49f1284e6075effde1fb5b5f2012c4b
Rostislav Svoboda <rosta.svo@gmail.com>
2026-04-16 10:44:28 +0200

    Simplify .idea rule in .gitignore

--- diff-utils/create_colorful_diff.sh
+++ diff-utils/create_colorful_diff.sh
@@ -28,7 +28,7 @@ cat <<EOF > $FILENAME
   <!-- jQuery-->
   <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
   <!-- Bootstrap JS -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"></script>
 
 <style type="text/css">
   body {
